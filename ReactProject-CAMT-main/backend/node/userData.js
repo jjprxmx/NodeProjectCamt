ce95db2d6a7859b9e4f81013de6865f8c23e5ebb
Ahhhhman <pawat.pooh@gmail.com>
2023-10-05 21:40:34 +0700

    Create userData.js

---
+++ ReactProject-CAMT-main/backend/node/userData.js
@@ -0,0 +1,52 @@
+const userData = [
+    {
+        id: 1,
+        username: "pim",
+        password: "1234",
+        name: "kide",
+        basket:
+            [
+                {
+                    id: 4,
+                    name: "ตัวแทนของท่านอ๋อง",
+                    author: "d",
+                    category: "kide",
+                    imgUrl: "ตัวแทนของท่านอ๋อง.jpeg"
+                },
+                {
+                    id: 5,
+                    name: "นางมาร",
+                    author: "e",
+                    category: "kide",
+                    imgUrl: "นางมาร.jpeg"
+                },
+            ],
+        level: "1"
+    },
+    {
+        id: 2,
+        username: "pim2",
+        password: "1234",
+        name: "kide",
+        basket:
+            [
+                {
+                    id: 6,
+                    name: "มหาเศรษฐีของโลกคือผมเอง",
+                    author: "f",
+                    category: "kide",
+                    imgUrl: "มหาเศรษฐีของโลกคือผมเอง.png"
+                },
+                {
+                    id: 3,
+                    name: "เซียนหมอซ้อนกล",
+                    author: "c",
+                    category: "kide",
+                    imgUrl: "เซียนหมอซ้อนกล.jpeg"
+                },
+            ],
+        level: "2"
+    },
+]
+
+module.exports = userData
