9fcc3ed6034cebec9a99d13753821f34446dde43
kanokwan-mahakham <119473607+kanokwan-mahakham@users.noreply.github.com>
2023-10-11 12:10:50 +0700

    kanokwan-11/10/2023

--- ReactProject-CAMT-main/backend/node/Routes/novels/novels.service.js
+++ ReactProject-CAMT-main/backend/node/Routes/novels/novels.service.js
@@ -1,35 +1,58 @@
-const novelData = require('../../novelData');
+const novelData = require("../../model/Novels");
 
-function findAllNovel(req,res){
-    try{
-        res.status(200).json(novelData);
-    }catch{
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    }
+function findAllNovel(req, res) {
+  try {
+    res.status(200).json(novelData);
+  } catch {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 }
 
-function findNovelById(req,res){
-    const novelID = Number.parseInt(req.params.id);
-    const novel = novelData.find((novel) => novel.id === novelID);
-    res.status(200).json(novel);
+function findNovelById(req, res) {
+  const novelID = Number.parseInt(req.params.id);
+  const novel = novelData.find((novel) => novel.id === novelID);
+  res.status(200).json(novel);
 }
 
 let currentNovelId = novelData.length;
-function createNovel(req,res){
-    const { name, author, category, imgUrl } = req.body;
-    const novel = {
-        id: ++currentNovelId,
-        name,
-        author,
-        category,
-        imgUrl
-    };
-    novelData.push(novel); 
-    res.status(200).json(novel);
+function createNovel(req, res) {
+  const {
+    name,
+    price,
+    category,
+    numpage,
+    rating,
+    promotion,
+    file_pic,
+    file_test,
+    file_real,
+    release_date,
+    author,
+    description,
+    cover_price,
+  } = req.body;
+  const novel = {
+    id: ++currentNovelId,
+    name,
+    price,
+    category,
+    numpage,
+    rating,
+    promotion,
+    file_pic,
+    file_test,
+    file_real,
+    release_date,
+    author,
+    description,
+    cover_price,
+  };
+  novelData.push(novel);
+  res.status(200).json(novel);
 }
 module.exports = {
-    findAllNovel,
-    findNovelById,
-    createNovel
-}
+  findAllNovel,
+  findNovelById,
+  createNovel,
+};
