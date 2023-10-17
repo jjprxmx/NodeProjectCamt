152ad3f904149df890c2efce7e76248ea173091e
Ahhhhman <pawat.pooh@gmail.com>
2023-10-18 00:13:41 +0700

    Update basket.service.js

--- ReactProject-CAMT-main/backend/node/Routes/baskets/basket.service.js
+++ ReactProject-CAMT-main/backend/node/Routes/baskets/basket.service.js
@@ -4,32 +4,30 @@ const Basket = require('../../model/Basket');
 const { data } = require('../../config/database');
 
 async function getAllBasket(req, res) {
-    const basket = await Basket.findAll();
-    if(!basket){
-        return res.status(404).json({message: 'Basket not found'});
-    }
-    res.status(200).send(basket);
+    res.status(200).send(Basket);
 }
 
 async function getBasket(req, res) {
-    const basket = await Basket.findOne({
-        id_user: req.params.id 
-    });
-    if(!basket){
-        return res.status(404).json({message: 'Basket not found'});
+    const basket = await Basket.findAll(
+        {
+            where: { id_user: req.params.id_user }
+
+        });
+    if (!basket) {
+        return res.status(404).json({ message: 'Basket not found' });
     }
     res.status(200).send(basket);
 }
 
 async function getItem(req, res) {
-    const basket = await Basket.findOne({
+    const basket = await Basket.findAll({
         where: {
             id_user: req.params.id_user,
             id_novel: req.params.id_novel
         }
     });
-    if(!basket){
-        return res.status(404).json({message: 'Basket not found'});
+    if (!basket) {
+        return res.status(404).json({ message: 'Basket not found' });
     }
     res.status(200).send(basket.id_novel);
 }
